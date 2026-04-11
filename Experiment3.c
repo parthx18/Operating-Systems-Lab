501a37d0679cd4e6c20801e556647e7a78db27c9
Parth V <parthvishnu1802@gmail.com>
2026-04-11 10:50:23 +0530

    Update and rename Experiment3 to Experiment3.c

--- Experiment3.c
+++ Experiment3.c
@@ -1,4 +1,4 @@
-1. Single Fork (2^1 = 2 processes)A single fork() creates one child. There are now 2 total processes.
+//1. Single Fork (2^1 = 2 processes)A single fork() creates one child. There are now 2 total processes.
 C#include <stdio.h>
 #include <unistd.h>
 
@@ -9,7 +9,7 @@ int main() {
 }
 
 
-2. Double Fork (2^2 = 4 processes)Two consecutive fork() calls result in 4 total processes.Fork 1: Creates 1 child (Total: 2).Fork 2: Both the original parent and the first child call fork (Total: 4).
+//2. Double Fork (2^2 = 4 processes)Two consecutive fork() calls result in 4 total processes.Fork 1: Creates 1 child (Total: 2).Fork 2: Both the original parent and the first child call fork (Total: 4).
 C#include <stdio.h>
 #include <unistd.h>
 
@@ -21,7 +21,7 @@ int main() {
 }
 
 
-3. Triple Fork (2^3 = 8 processes)Three consecutive fork() calls result in 8 total processes. This creates a "tree" structure where the original parent eventually has several generations of descendants.
+//3. Triple Fork (2^3 = 8 processes)Three consecutive fork() calls result in 8 total processes. This creates a "tree" structure where the original parent eventually has several generations of descendants.
 C#include <stdio.h>
 #include <unistd.h>
 
@@ -34,9 +34,10 @@ int main() {
     return 0;
 }
 
-
+/*
 Number of Forks,Total Processes,New Processes Created
 1 (Single),2,1
 2 (Double),4,3
 3 (Triple),8,7
 n,2n,2n−1
+*/
